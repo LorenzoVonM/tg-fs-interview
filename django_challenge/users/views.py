0fde1f6818fd257ea482aec63e48c7e5ea87f195
Antonio Rodriguez <arodriguez@kea.mx>
2025-06-18 11:22:34 -0600

    First commit with django and react challenge

--- django_challenge/users/views.py
+++ django_challenge/users/views.py
@@ -5,6 +5,39 @@ from .serializers import UserSerializer
 
 class UserListAPIView(APIView):
     def get(self, request):
-        users = User.objects.all()  # Tu lógica aquí
+        # Min age, get the param in the request
+        min_age = request.query_params.get('min_age')
+
+        # Filter all user by date_joined field desc
+        users = User.objects.all().order_by('-date_joined')
+
+        # if the min_age exist 
+        if min_age is not None:
+            try:
+                min_age = int(min_age)
+                users = users.filter(age__gte=min_age)
+            except ValueError:
+                except Response('error: min_age must be integer', status=400)
+        
         serializer = UserSerializer(users, many=True)
+
+        # Get the firts_name and last_name from the request
+        first_name = serializer.data['first_name']
+        last_name = serializer.data['last_name']
+        full_name = first_name + last_name
+
+        # add the full name field to the request
+        serializer.data['full_name'] = full_name
+
+        # checking if the age is higher or equals to 18
+        age = serializer.data['age']
+        if age >= 18:
+            is_adult = True
+        else:
+            is_adult = False
+
+        # Add the is_Adult filed to the request if tre
+        serializer.data['is_Adult'] = is_adult
+
         return Response(serializer.data)
+    
--- react_challenge/src/App.jsx
+++ react_challenge/src/App.jsx
@@ -1,11 +1,67 @@
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import UserList from "./components/UserList";
 
+
 function App() {
+
+    // States
+    const [users, setUsers] = useState([])
+    const [minAge, setMinAge] = useState(0)
+
+    // Loading state
+    const [loading, setLoading] = useState(false)
+
+    // Funtion to get users from API
+    const fetchUsers = async () => {
+        setLoading(true)
+        try {
+            // fetch users with query param 'minAge'
+            const response = await fetch('http:localhost:8000/api/users/?min_age=${minAge}')
+            
+            // get data in json format
+            const data = await response.json()
+            setUsers(data.users) // Supouse the API return { users []}
+        } catch (error) {
+            console.error('No se pudo obtener la lista de usuarios.')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
   return (
     <div>
-      <h1>Usuarios</h1>
+      <h1>Lista de usuarios</h1>
+      <input
+        type="number"
+        value={minAge}
+        onChange={(e) => setMinAge(e.target.value)}
+        />
+
+        <button onClick={fetchUsers}> Filtrar </button>
+
+        {/* Chechking loading state */}
+        {loading ? ( 
+            <p>Cargando...</p>
+        ) : 
+        <ul>
+
+            {users.map((user) => (
+                <li key={user.full_name}>
+                    {user.full_name} - {user.age} años - {user.is_Adult ? '✅' : '❌'} 
+
+                </li>
+            ))}
+        </ul>
+        
+        }
+
+
+
       <UserList />
     </div>
   );
